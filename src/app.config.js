b7aed9ec834604c07ae02a9bd8b772a847cc9c67
xiekecheng <xkccoding@gmail.com>
2021-09-05 23:26:58 +0800

    init

---
+++ src/app.config.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: xiekc
+ * @Date: 2021-09-05 01:32:06
+ * @LastEditors: xkccoding@gmail.com
+ * @LastEditTime: 2021-09-05 23:21:57
+ * @FilePath: /react-taro-blisscake/src/app.config.js
+ */
+export default {
+  pages: [
+    'pages/home/index',
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
